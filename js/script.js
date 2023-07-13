fcde9c1e7b6804f34ba95038bbeb2d0449cfe141
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-07-13 14:02:14 +0100

    add 'showMessage' function and change way of search

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 {
 	const searchInput = document.querySelector('.search');
 	const books = document.querySelectorAll('.book-list li');
-	const searchBtn = document.querySelector('.searchBtn');
+	const messageContainer = document.querySelector('.book-list ul');
+	const msg = document.createElement('p');
+
+	const showMessage = function () {
+		msg.textContent = 'Brak książki w bazie!';
+		messageContainer.appendChild(msg);
+	};
 
 	const startSearch = function () {
 		const text = searchInput.value.toLocaleLowerCase();
-
+		let booksArr = [];
 		for (let book of books) {
 			const item = book.textContent.toLocaleLowerCase();
 			if (item.indexOf(text) === -1) {
 				book.classList.add('hide');
+				console.log('znalezione ksiazki' + booksArr.length);
 			} else {
 				book.classList.remove('hide');
+				booksArr.push(book);
+				console.log('znalezione ksiazki' + booksArr.length);
 			}
 		}
+		if (booksArr.length == 0) {
+			showMessage();
+		} else {
+			msg.remove();
+		}
+		console.log(books.length);
 	};
 
-	searchBtn.addEventListener('click', startSearch);
+	searchInput.addEventListener('keyup', startSearch);
 }
